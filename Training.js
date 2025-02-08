173c701d4208682a491e15317d89f0deae37ef16
Eins208 <105994100+Eins208@users.noreply.github.com>
2025-02-08 07:59:22 +0300

    Add files via upload

---
+++ Training.js
@@ -0,0 +1,12 @@
+// Задание 1.Вывести на экран Hello, world!
+console.log('Hello, World!'); 
+// Задание 2.Написать однострочный комментарий с текстом:You know nothing, Jon Snow!.
+//You know nothing, Jon Snow!.
+// Задание 3. Вывести на экран три имени: Robert, Stannis, Renly, используя: console.log
+console.log('Robert');
+console.log('Stannis');
+console.log('Renly');
+//Задание 4.Ввести на экран 9780262531962
+console.log(9780262531962); 
+// Задание 5. Вывести на экран What Is Dead May Never Die
+console.log('What Is Dead May Never Die');
